f140e0204ae47a878a9794e01b5be0f859615cbd
Cameron Embree <CSEmbree@gmail.com>
2013-10-16 23:42:47 -0700

    templates for deafult mains

---
+++ templates/template_java.java
@@ -0,0 +1,13 @@
+
+
+import java.io.*; 
+import java.util.*;
+
+
+class MyFirstProgram 
+{
+	public static void main(String[] args)
+	{
+	
+	}
+}
---
+++ templates/template_python.py
@@ -0,0 +1,25 @@
+
+
+import sys, string, time
+
+
+def main():
+	print 'Hello World'
+
+	Display('Hello Again')
+
+
+def Display(text):
+	print 'told to display: ', text
+
+
+
+
+
+
+
+
+
+#-------------------------------
+if __name__ == "__main__":
+    main()
