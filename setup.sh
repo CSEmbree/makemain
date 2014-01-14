61208326e2e821eb714606b452c7781ea2703785
Cameron Embree <CSEmbree@gmail.com>
2014-01-13 19:32:24 -0800

    Creates wrapper script that calls executable. Allows for setup of user prefered envrionment.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+#creates the wrapper script to run makemain with appropreate envrionment variables for settings
 
-echo "mm:: Please enter a permanent author name: "
+echo "#!/bin/bash" > mm
+echo "" >> mm
 
-read authorName
 
-export MM_AUTHOR="${authorName}"
+#all the arguments passed to the wrapper script need to be passed to the executable
+echo "ARGS=\"\"" >> mm 
+echo "SPACE=\" \"" >> mm
 
-echo "mm:: ran: export MM_AUTHOR=\"$authorName\""
-echo "mm:: Author name '$authorName' set! makemain is ready to use."
+#make a copy of all arguments to be passed to make main executable
+echo "for arg in \"\$@\"" >> mm
+echo "do" >> mm
+echo "    ARGS=\$ARGS\$SPACE\$arg" >> mm
+echo "done" >> mm
+
+
+#check for setting an author by default every time
+echo "mm:: Want to enter a permanent author name?: [Y/n]"
+read ans
+	
+if [ "$ans" == "Y" ]; then
+	#save author preferences into wrapper script
+    echo "mm:: Please enter a permanent author name: "
+    read authorName
+    echo "mm:: Author name set to: '$authorName'."
+    echo "export MM_AUTHOR=\"${authorName}\" " >> mm
+else
+	#incorrect response will be assumed as a negative answer. Report it to user for clairity.
+	echo "mm:: Your answer: '$ans'."
+	echo "mm:: No permanent author name option selected!"
+fi
+
+#always export the executable path location for running makemain
+echo "export MM_PATH=\"$(pwd)/bin/makemain\" " >> mm
+echo "exec \${MM_PATH} \$ARGS" >> mm
+
+
+#move the wrapper script to a standard execution path to allow makemain to be run from anywhere
+chmod +x mm
+mv mm /usr/local/bin/mm
